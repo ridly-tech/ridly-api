fd84ad35dfafe5fe51d1090b7b41a7cb2991109a
Daniel Lord-Doyle <daniel@Daniels-MBP-2.modem>
2022-07-14 12:37:42 +1000

    refactored schema and moved User queries in to its own graphql folder

---
+++ src/graphql/User.ts
@@ -0,0 +1,330 @@
+import { APP_SECRET, getUserId } from '../utils'
+import { compare, hash } from 'bcryptjs'
+import { sign } from 'jsonwebtoken'
+import {
+  intArg,
+  nonNull,
+  objectType,
+  stringArg,
+  inputObjectType,
+  arg,
+  asNexusMethod,
+  enumType,
+  booleanArg,
+} from 'nexus'
+import { DateTimeResolver } from 'graphql-scalars'
+import { Context } from './context'
+
+export const DateTime = asNexusMethod(DateTimeResolver, 'date')
+
+export const Query = objectType({
+  name: 'Query',
+  definition(t) {
+    t.nonNull.list.nonNull.field('allUsers', {
+      type: 'User',
+      resolve: (_parent, _args, context: Context) => {
+        return context.prisma.user.findMany()
+      },
+    })
+
+    t.nullable.field('me', {
+      type: 'User',
+      resolve: (parent, args, context: Context) => {
+        const userId = getUserId(context)
+        return context.prisma.user.findUnique({
+          where: {
+            id: String(userId),
+          },
+        })
+      },
+    })
+
+    t.nullable.field('postById', {
+      type: 'Post',
+      args: {
+        id: stringArg(),
+      },
+      resolve: (_parent, args, context: Context) => {
+        return context.prisma.post.findUnique({
+          where: { id: args.id || undefined },
+        })
+      },
+    })
+
+    t.nonNull.list.nonNull.field('feed', {
+      type: 'Post',
+      args: {
+        searchString: stringArg(),
+        skip: intArg(),
+        take: intArg(),
+        orderBy: arg({
+          type: 'PostOrderByUpdatedAtInput',
+        }),
+      },
+      resolve: (_parent, args, context: Context) => {
+        const or: any = args.searchString
+          ? {
+            OR: [
+              { title: { contains: args.searchString } },
+              { content: { contains: args.searchString } },
+            ],
+          }
+          : {}
+
+        return context.prisma.post.findMany({
+          where: {
+            published: true,
+            ...or,
+          },
+          take: args.take || undefined,
+          skip: args.skip || undefined,
+          orderBy: args.orderBy || undefined,
+        })
+      },
+    })
+
+    t.list.field('draftsByUser', {
+      type: 'Post',
+      args: {
+        userUniqueInput: nonNull(
+          arg({
+            type: 'UserUniqueInput',
+          }),
+        ),
+      },
+      resolve: (_parent, args, context: Context) => {
+        return context.prisma.user
+          .findUnique({
+            where: {
+              id: args.userUniqueInput.id || undefined,
+              email: args.userUniqueInput.email || undefined,
+            },
+          })
+          .posts({
+            where: {
+              published: false,
+            },
+          })
+      },
+    })
+  },
+})
+
+export const Mutation = objectType({
+  name: 'Mutation',
+  definition(t) {
+    t.field('signup', {
+      type: 'AuthPayload',
+      args: {
+        name: stringArg(),
+        email: nonNull(stringArg()),
+        password: nonNull(stringArg()),
+        isAdmin: booleanArg()
+      },
+      resolve: async (_parent, args, context: Context) => {
+        const hashedPassword = await hash(args.password, 10)
+        const user = await context.prisma.user.create({
+          data: {
+            name: args.name,
+            email: args.email,
+            password: hashedPassword,
+            isAdmin: args.isAdmin ?? false
+          },
+        })
+        return {
+          token: sign({ userId: user.id }, APP_SECRET),
+          user,
+        }
+      },
+    })
+
+    t.field('login', {
+      type: 'AuthPayload',
+      args: {
+        email: nonNull(stringArg()),
+        password: nonNull(stringArg()),
+      },
+      resolve: async (_parent, { email, password }, context: Context) => {
+        const user = await context.prisma.user.findUnique({
+          where: {
+            email,
+          },
+        })
+        if (!user) {
+          throw new Error(`No user found for email: ${email}`)
+        }
+        const passwordValid = await compare(password, user.password)
+        if (!passwordValid) {
+          throw new Error('Invalid password')
+        }
+        return {
+          token: sign({ userId: user.id }, APP_SECRET),
+          user,
+        }
+      },
+    })
+
+    t.field('createDraft', {
+      type: 'Post',
+      args: {
+        data: nonNull(
+          arg({
+            type: 'PostCreateInput',
+          }),
+        ),
+      },
+      resolve: (_, args, context: Context) => {
+        const userId = getUserId(context)
+        return context.prisma.post.create({
+          data: {
+            title: args.data.title,
+            content: args.data.content,
+            authorId: userId,
+          },
+        })
+      },
+    })
+
+    t.field('togglePublishPost', {
+      type: 'Post',
+      args: {
+        id: nonNull(stringArg()),
+      },
+      resolve: async (_, args, context: Context) => {
+        try {
+          const post = await context.prisma.post.findUnique({
+            where: { id: args.id || undefined },
+            select: {
+              published: true,
+            },
+          })
+          return context.prisma.post.update({
+            where: { id: args.id || undefined },
+            data: { published: !post?.published },
+          })
+        } catch (e) {
+          throw new Error(
+            `Post with ID ${args.id} does not exist in the database.`,
+          )
+        }
+      },
+    })
+
+    t.field('incrementPostViewCount', {
+      type: 'Post',
+      args: {
+        id: nonNull(stringArg()),
+      },
+      resolve: (_, args, context: Context) => {
+        return context.prisma.post.update({
+          where: { id: args.id || undefined },
+          data: {
+            viewCount: {
+              increment: 1,
+            },
+          },
+        })
+      },
+    })
+
+    t.field('deletePost', {
+      type: 'Post',
+      args: {
+        id: nonNull(stringArg()),
+      },
+      resolve: (_, args, context: Context) => {
+        return context.prisma.post.delete({
+          where: { id: args.id },
+        })
+      },
+    })
+  },
+})
+
+export const User = objectType({
+  name: 'User',
+  definition(t) {
+    t.string('id')
+    t.string('name')
+    t.string('email')
+    t.nonNull.list.nonNull.field('posts', {
+      type: 'Post',
+      resolve: (parent, _, context: Context) => {
+        return context.prisma.user
+          .findUnique({
+            where: { id: parent.id || undefined },
+          })
+          .posts()
+      },
+    })
+  },
+})
+
+export const Post = objectType({
+  name: 'Post',
+  definition(t) {
+    t.nonNull.string('id')
+    t.nonNull.field('createdAt', { type: 'DateTime' })
+    t.nonNull.field('updatedAt', { type: 'DateTime' })
+    t.nonNull.string('title')
+    t.string('content')
+    t.nonNull.boolean('published')
+    t.nonNull.int('viewCount')
+    t.field('author', {
+      type: 'User',
+      resolve: (parent, _, context: Context) => {
+        return context.prisma.post
+          .findUnique({
+            where: { id: parent.id || undefined },
+          })
+          .author()
+      },
+    })
+  },
+})
+
+export const SortOrder = enumType({
+  name: 'SortOrder',
+  members: ['asc', 'desc'],
+})
+
+export const PostOrderByUpdatedAtInput = inputObjectType({
+  name: 'PostOrderByUpdatedAtInput',
+  definition(t) {
+    t.nonNull.field('updatedAt', { type: 'SortOrder' })
+  },
+})
+
+export const UserUniqueInput = inputObjectType({
+  name: 'UserUniqueInput',
+  definition(t) {
+    t.string('id')
+    t.string('email')
+  },
+})
+
+export const PostCreateInput = inputObjectType({
+  name: 'PostCreateInput',
+  definition(t) {
+    t.nonNull.string('title')
+    t.string('content')
+  },
+})
+
+export const UserCreateInput = inputObjectType({
+  name: 'UserCreateInput',
+  definition(t) {
+    t.nonNull.string('email')
+    t.string('name')
+    t.list.nonNull.field('posts', { type: 'PostCreateInput' })
+    t.nullable.boolean('isAdmin')
+  },
+})
+
+export const AuthPayload = objectType({
+  name: 'AuthPayload',
+  definition(t) {
+    t.string('token')
+    t.field('user', { type: 'User' })
+  },
+})
